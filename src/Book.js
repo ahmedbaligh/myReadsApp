732d84d366ae17ddc8629de0369fcce78e064b20
Ahmed Baligh <ahmedm11@hotmail.com>
2020-12-08 21:47:26 +0200

    added Book component to render each book

---
+++ src/Book.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+
+class Book extends Component {
+  handleChange = evt => {
+      this.props.onShelfChange && this.props.onShelfChange(this.props.book, evt.target.value);
+  }
+
+  render() {
+    const book = this.props.book;
+    return (
+      <li>
+        <div className='book'>
+          <div className='book-top'>
+            <div
+              className='book-cover'
+              style={{ backgroundImage: `url(${book.imageLinks && book.imageLinks.thumbnail})` }}
+            >
+            </div>
+            
+            <div className='book-shelf-changer'>
+              <select
+                value={book.shelf || ''}
+                onChange={this.handleChange}
+              >
+                <option value='' disabled>Move to...</option>
+                <option value='currentlyReading'>Currently Reading</option>
+                <option value='wantToRead'>Want to Read</option>
+                <option value='read'>Read</option>
+                <option value='none'>None</option>
+              </select>
+            </div>
+          </div>
+          <div className='book-title'>
+            {book.title}
+          </div>
+          <div>
+            {book.authors && book.authors.map((author, i) => (
+              <div key={i} className='book-authors'>
+                {author}
+              </div>
+            ))}
+          </div>
+        </div>
+      </li>
+    )
+  }
+}
+
+export default Book;
