f4e9665aa00d58f41924d76124e028e88b60ed2f
Ahmed Baligh <ahmedm11@hotmail.com>
2020-12-08 21:12:50 +0200

    implemented Search functionality

---
+++ src/BookSearch.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Book from './Book';
+
+class BookSearch extends Component {
+
+  state = {
+    query: ''
+  }
+
+  handleSearch = query => {
+    this.setState({ query });
+    this.props.onSearch(query);
+  }
+
+  clearQuery = () => this.updateQuery('');
+
+  render() {
+
+    const { query } = this.state;
+    const { books, onShelfChange } = this.props;
+
+    return (
+      <div className='search-books'>
+        <div className='search-books-bar'>
+          <Link
+            className='close-search'
+            to='/'
+          >
+            Close
+          </Link>
+          
+          <div className='search-books-input-wrapper'>
+            <input 
+              id='search-input'
+              type='text'
+              placeholder='Search Books'
+              autoFocus
+              value={query}
+              onChange={evt => this.handleSearch(evt.target.value)}
+            />
+          </div>
+        </div>
+
+        <div className='search-books-results'>
+          {books.length > 0 && (
+            <div className='search-books-results-count'>
+              Found {books.length} Results
+            </div>
+          )}
+          <ol className='books-grid'>
+            {books.length ?
+              (books.map(book => (
+                <Book
+                  key={book.id}
+                  book={book}
+                  onShelfChange={onShelfChange}
+                />
+              )))
+              :
+              <div>No Results Found</div>
+            }
+          </ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+export default BookSearch;
